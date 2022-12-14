397a65801d7b09f9b807015a4ac4719516d7ea67
turanayhan <duranayhan57@gmail.com>
2022-12-14 15:30:55 +0300

    version 7

---
+++ src/Player.py
@@ -0,0 +1,17 @@
+from src.Person import Person
+
+
+class Player(Person):
+    def __init__(self, gsm, address, education_status, job, mail, acting_education, experience,
+                 name, surname, age, gender, identification_number, size, weight, hair_color, eye_color,
+                 marital_status, date_of_birth):
+        super().__init__(self, name, surname, age, gender, identification_number, size, weight, hair_color, eye_color,
+                         marital_status, date_of_birth)
+
+        self.gsm = gsm
+        self.address = address
+        self.education_status = education_status
+        self.job = job
+        self.mail = mail
+        self.acting_education = acting_education
+        self.experience = experience
--- src/PlayerRegistration.py
+++ src/PlayerRegistration.py
@@ -1,17 +1,8 @@
-from src.Person import Person
+from src.Player import Player
+from src.Experience import Experience
+class PlayerRegistration:
+    def __init__(self,player):
+
+    
 
 
-class PlayerRegistration(Person):
-    def __init__(self, gsm, address, education_status, job, mail, acting_education, experience,
-                 name, surname, age, gender, identification_number, size, weight, hair_color, eye_color,
-                 marital_status, date_of_birth):
-        super().__init__(self, name, surname, age, gender, identification_number, size, weight, hair_color, eye_color,
-                         marital_status, date_of_birth)
-
-        self.gsm = gsm
-        self.address = address
-        self.education_status = education_status
-        self.job = job
-        self.mail = mail
-        self.acting_education = acting_education
-        self.experience = experience
