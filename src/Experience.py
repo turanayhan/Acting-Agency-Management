a1fba6695b614330a76bbe0eeaa3cd4f8a06580c
turanayhan <duranayhan57@gmail.com>
2022-12-14 15:20:45 +0300

    version 6

---
+++ src/Experience.py
@@ -0,0 +1,5 @@
+class Experience:
+    def __init__(self, cinema, dizi, theatre):
+        self.cinema = cinema
+        self.dizi = dizi
+        self.theatre = theatre
--- src/PlayerRegistration.py
+++ src/PlayerRegistration.py
@@ -2,10 +2,7 @@ from src.Person import Person
 
 
 class PlayerRegistration(Person):
-    education_information = "yok"
-    experience_information = "yok"
-
-    def __init__(self, gsm, address, education_status, job, mail, acting_education, acting_experience,
+    def __init__(self, gsm, address, education_status, job, mail, acting_education, experience,
                  name, surname, age, gender, identification_number, size, weight, hair_color, eye_color,
                  marital_status, date_of_birth):
         super().__init__(self, name, surname, age, gender, identification_number, size, weight, hair_color, eye_color,
@@ -17,4 +14,4 @@ class PlayerRegistration(Person):
         self.job = job
         self.mail = mail
         self.acting_education = acting_education
-        self.acting_experience = acting_experience
+        self.experience = experience
